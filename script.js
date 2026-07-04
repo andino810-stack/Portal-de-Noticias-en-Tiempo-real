cd5a6c4cb91b5aa3a8f7f1d555bb28f3f614ac90
andino810-stack <andino810@gmail.com>
2026-07-04 14:34:23 -0300

    Remove Alem News RSS feed from the list

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let noticias = [];
 
 const rss = [
-["Alem News","https://www.alem.news/feed/"],
 ["Clarín","https://www.clarin.com/rss/lo-ultimo/"],
 ["Infobae","https://www.infobae.com/feeds/rss/"],
 ["BBC Mundo","https://feeds.bbci.co.uk/mundo/rss.xml"],
